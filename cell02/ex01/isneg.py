189872e32971f1bb5c7c47e79f99b8f8ff336e2e
Tichakon21 <Tichakont@gmail.com>
2025-06-17 08:32:31 +0000

    edit

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+value = int ("0")
+if value > 0:
+	print("This number is positive.")
+elif value < 0:
+    print("this number is negative.")
+else:
+    print("is number is both negative and positive.")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "python is awsome"
+prompt = input()
+if prompt == password :
+   print("ACCESS GRANTED")
+else:
+    print("ACCESS GRANTED")
